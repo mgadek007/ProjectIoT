3b976bc7983579c69bc1b7b3a1d3330612110b22
krycho23 <kbytnar@gmail.com>
2019-04-01 15:58:34 +0200

    add application module

---
+++ application/src/main/java/users/Administrator.java
@@ -0,0 +1,22 @@
+package users;
+
+public class Administrator extends User{
+    private String password;
+
+    public void setTemperature(){
+
+    }
+
+    public void setWindowsPos(){
+
+    }
+
+    public void setAC(){
+
+    }
+
+    public void setLights(){
+
+    }
+
+}
---
+++ application/src/main/java/users/User.java
@@ -0,0 +1,14 @@
+package users;
+
+public class User {
+    protected int pref_temp;
+    protected boolean AC;
+    protected boolean windows;
+    protected String login;
+    protected String name;
+    protected String last_name;
+
+    public void setPreferences(){
+
+    }
+}
